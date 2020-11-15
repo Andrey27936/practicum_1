fb78417106d3a95eaf75fda5c28313d75b22c2fd
Andrey27936 <73572736+Andrey27936@users.noreply.github.com>
2020-11-15 15:19:01 +0500

    Add files via upload

---
+++ Задача 3.py
@@ -0,0 +1,19 @@
+"""
+ Имя проекта: practicum_1
+ Номер версии: 1.0
+ Имя файла: Задача 1.ру
+ Автор: 2020 © А.И. Баскаков, Челябинск
+ Лицензия использования: CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/deed.ru)
+ Дата создания: 15/11/2020
+ Связанные файлы/пакеты: numpy, random
+ Описание: Задача 3.
+ #версия Python: 3.9
+"""
+import math
+a = 3
+b = 4
+c = 5
+p = (a+b+c) / 2
+S = math.sqrt( p * (p-a) * (p-b) * (p-c))
+print(p * 2)
+print(S)
